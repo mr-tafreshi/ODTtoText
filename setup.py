3311d51b01d243d2c97a7804784dc21e2be2e440
KaneGalba <kane@galba.co>
2017-05-04 21:17:48 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='ODTReader',
+    version='0.0.1',
+    url='https://github.com/KaneGalba/ODTReader/',
+    download_url='https://github.com/KaneGalba/ODTReader/archive/master.zip',
+    author='Kane Galba',
+    author_email='kane@galba.co',
+    description='Lightweight python module to allow extracting text from OpenDocument (odt) files.',
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    license='GPL',
+    install_requires=[],
+)
